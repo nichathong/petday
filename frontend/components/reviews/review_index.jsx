79f1b93a1709d88c9bbb1149101de1b01ef9d81e
nichathong <nicha.fn@gmail.com>
2021-10-22 11:13:07 -0700

    added review index item

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -2,19 +2,45 @@ import React from 'react';
 import ReviewIndexItem from './review_index_item';
 
 class ReviewIndex extends React.Component {
-    componentDidMount() {
-        this.props.fetchReviews(this.props.businessId)
-    }
+  constructor(props) {
+      super(props);
+    //   this.update = false; 
+  }  
+  componentDidMount() {
+    this.props.fetchReviews(this.props.businessId);
+  }
 
-    render () {
-        return (
-            <div className="review-index-container">
-                <ul>
-                    {this.props.reviews.map( review => <ReviewIndexItem key={review.id} review={review}/>)}
-                </ul>
-            </div>
-        )
-    }
+//   componentDidUpdate() {
+//     if (
+//       this.props.business.reviews.length !==
+//         Object.values(this.props.reviews).length &&
+//       !this.update
+//     ) {
+//       this.update = true;
+//       this.props.fetchReviews(this.props.business.id);
+//     }
+//   }
+
+  render() {
+    return (
+      <div className="review-index-container">
+        <ul>
+          {this.props.reviews.map((review) => (
+            <ReviewIndexItem
+              key={review.id}
+              review={review}
+              fetchUsers={this.props.fetchUsers}
+              currentUser={this.props.currentUser}
+              users={this.props.users}
+              updateReview={this.props.updateReview}
+              deleteReview={this.props.deleteReview}
+              business={this.props.business}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ReviewIndex;
--- frontend/components/reviews/review_index_item.jsx
+++ frontend/components/reviews/review_index_item.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 
 class ReviewIndexItem extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.ratingStar = "";
+        if (this.props.review.rating === 1) {
+            this.ratingStar = window.oneStar;
+        } else if (this.props.review.rating === 2) {
+            this.ratingStar = window.twoStar;
+        } else if (this.props.review.rating === 3) {
+            this.ratingStar = window.threeStar;
+        } else if (this.props.review.rating === 4) {
+            this.ratingStar = window.fourStar;
+        } else {
+            this.ratingStar = window.fiveStar;
+        }
+        this.date = new Date(this.props.review.created_at);
+        this.realDate = (this.date.getMonth() + 1) + "/" + this.date.getDate() + "/" + this.date.getFullYear();
+        this.handleDelete = this.handleDelete.bind(this);
 
     }
 
     handleDelete(e) {
         e.preventDefault();
-        this.props.deleteReview(this.props.review, this.props.businessId);
+        this.props.deleteReview(this.props.review, this.props.business.id);
+    }
+
+    componentDidMount() {
+        this.props.fetchUsers();
     }
 
 
@@ -16,7 +35,32 @@ class ReviewIndexItem extends React.Component {
         const { review, currentUser } = this.props;
         return (
             <div>
-                here is your reviews
+                <div className="review-container">
+                    <div className="review-profile-container">
+                        <div className="profile-box">
+                            <div className="user-info">
+                                <li className="user-username">{this.props.users[review.user_id].firstname.slice(0, 1).toUpperCase() + this.props.users[review.user_id].firstname.slice(1)} {this.props.users[review.user_id].lastname.slice(0,1).toUpperCase()}.</li>
+                                <li>{this.props.users[review.user_id].zip_code}</li>
+                            </div>
+                        </div>
+                        <div className="rating-date-container">
+                            <div className="review-rating">
+                                <img src={this.ratingStar} alt="" />
+                            </div>
+                            <div className="review-created-date">
+                                {this.realDate}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="review-message-container">
+                        <div className="review-message-box">
+                            {this.props.review.message}
+                        </div>
+                    </div>
+
+
+                </div>
+                
             </div>
         )
     }
