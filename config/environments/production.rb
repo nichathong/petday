8059c2e51011b0f59079a9480340b0cbdd597cfd
nichathong <nicha.fn@gmail.com>
2024-03-29 13:22:12 -0700

    Try to deloy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true # ***change from false to true **** --> pipeline issues
+  config.assets.compile = false # ***change from false to true **** --> pipeline issues
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
